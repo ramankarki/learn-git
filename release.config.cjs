8a2c54fb09a5d453c6755630c7dc601ca7918018
ramankarki <ramankarki40@gmail.com>
2024-08-19 14:36:33 +0545

    fix: use @semantic-release/exec to auto update package.json version

--- release.config.cjs
+++ release.config.cjs
@@ -10,9 +10,14 @@ module.exports = {
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
-		'@semantic-release/npm',
 		'@semantic-release/changelog',
-
+		[
+			'@semantic-release/exec',
+			{
+				prepareCmd:
+					"node -e \"let pkg=require('./package.json');pkg.version='${nextRelease.version}';require('fs').writeFileSync('./package.json', JSON.stringify(pkg, null, 2));\"",
+			},
+		],
 		[
 			'@semantic-release/git',
 			{
