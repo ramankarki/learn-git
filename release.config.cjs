d5f13983e8dcb7e33b7ee6934f80f2c3067c1f56
ramankarki <ramankarki40@gmail.com>
2024-08-19 14:28:28 +0545

    fix: auto update package.json

--- release.config.cjs
+++ release.config.cjs
@@ -10,8 +10,9 @@ module.exports = {
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
+		'@semantic-release/npm',
 		'@semantic-release/changelog',
-		'@semantic-release/github',
+
 		[
 			'@semantic-release/git',
 			{
@@ -20,5 +21,7 @@ module.exports = {
 					'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
 			},
 		],
+
+		'@semantic-release/github',
 	],
 }
